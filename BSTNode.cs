7aa7ecebaac1704022182a69116f440245330678
DimaKirejko <iwerlandowawww1@gmail.com>
2023-12-26 14:06:28 +0200

    commit for task 4

---
+++ BSTNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba04
+{
+    internal class BSTNode
+    {
+        public int Key { get; set; }
+        public string Value { get; set; }
+        public BSTNode Left { get; set; }
+        public BSTNode Right { get; set; }
+
+        public BSTNode(int key, string value)
+        {
+            Key = key;
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+}
